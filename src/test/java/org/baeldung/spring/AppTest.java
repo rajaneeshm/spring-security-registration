a12e7ea0bf322c4f821991a49c79aeec3e575b94
rajaneeshm <rajaneesh72@gmail.com>
2017-12-16 19:42:22 +0530

    Test case for boostraping application

--- src/test/java/org/baeldung/spring/AppTest.java
+++ src/test/java/org/baeldung/spring/AppTest.java
@@ -1,34 +1,51 @@
 package org.baeldung.spring;
 
-import org.junit.Before;
+import org.baeldung.Application;
+import org.baeldung.web.controller.RegistrationCaptchaController;
+import org.baeldung.web.controller.RegistrationController;
+import org.baeldung.web.controller.UserController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AppTest {
 
-	private MockMvc mockMvc;
+	//private MockMvc mockMvc;
 
-	@Before
+	/*@Before
 	public void setUp() {
-		mockMvc = MockMvcBuilders.standaloneSetup(new IndexController()).build();
-	}
+		//mockMvc = MockMvcBuilders.standaloneSetup(new IndexController()).build();
+	}*/
+	
+	@Autowired
+	RegistrationController registrationController;
+	
+	@Autowired
+	UserController userController;
+	
+	@Autowired
+	RegistrationCaptchaController registrationCaptchaController;
+	
 
 	@Test
 	public void contextLoads() throws Exception {
 
-		this.mockMvc.perform(MockMvcRequestBuilders.get("/"))
+		new SpringApplicationBuilder().sources(Application.class).build();
+		Assert.assertNotNull(registrationController);
+		Assert.assertNotNull(userController);
+		Assert.assertNotNull(registrationCaptchaController);
+		
+		/*this.mockMvc.perform(MockMvcRequestBuilders.get("/"))
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.view().name("index"))
-				.andDo(MockMvcResultHandlers.print());
+				.andDo(MockMvcResultHandlers.print());*/
 
 	}
 
